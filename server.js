f747557a4e1fadb048134d9773e124fcf4ea93bf
Platon <111748152+payton-ggg@users.noreply.github.com>
2024-09-14 13:20:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const tonViewerUrl =
 // Парсинг данных с сайта DeDust
 async function getPoolData() {
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: "new",
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
